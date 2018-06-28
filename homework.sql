c47e082b2470028634158b92f873c06047432f03
Connell, Brian <brian_connell@baxter.com>
2018-06-27 20:19:46 -0500

    update

--- homework.sql
+++ homework.sql
@@ -6,46 +6,55 @@ SELECT upper(CONCAT(first_name,' ',last_name)) as 'Actor Name' FROM actor ;
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
 SELECT actor_id, first_name,last_name FROM actor
 WHERE first_name = 'Joe';
+
 -- 2b. Find all actors whose last name contain the letters GEN:
 SELECT actor_id, first_name,last_name FROM actor
 WHERE last_name LIKE '%GEN%';
 -- 2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
-
--- TO DO **************************
 SELECT actor_id, first_name,last_name FROM actor
 WHERE last_name LIKE '%LI%'
-GROUP BY last_name,first_name;
+ORDER BY last_name, first_name;
 -- 2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
-SELECT country_id, country FROM country
+SELECT country_id, country
+FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh','China');
 -- 3a. Add a middle_name column to the table actor. Position it between first_name and last_name. Hint: you will need to specify the data type.
--- TO DO ************************** how to order?
 ALTER TABLE actor
-ADD COLUMN middle_name VARCHAR(30); 
--- 3b. You realize that some of these actors have tremendously long last names. Change the data type of the middle_name column to blobs.
+ADD COLUMN middle_name VARCHAR(30) AFTER first_name;
 
+SELECT * FROM actor;
+-- 3b. You realize that some of these actors have tremendously long last names. Change the data type of the middle_name column to blobs.
 ALTER TABLE actor
-ALTER COLUMN middle_name VARCHAR(30);
+MODIFY COLUMN middle_name BLOB;
+
 -- 3c. Now delete the middle_name column.
 ALTER TABLE actor
-DELETE COLUMN middle_name;
+DROP COLUMN middle_name;
 -- 4a. List the last names of actors, as well as how many actors have that last name.
-SELECT last_name, len(last_name) FROM actor
-GROUP BY DISTINCT last_name
+SELECT last_name, COUNT(last_name)
+FROM actor
+GROUP BY  last_name;
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
-SELECT last_names, COUNT(last_name) FROM actor
-GROUP BY DISTINCT last_name
-HAVING COUNT(last_name) >2;
+SELECT last_name, COUNT(last_name)
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) >=2;
 -- 4c. Oh, no! The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
 UPDATE actor
 SET first_name ='HARPO' AND last_name = 'WILLIAMS'
 WHERE first_name ='GROUCHO' AND last_name = 'WILLIAMS' 
 
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO. Otherwise, change the first name to MUCHO GROUCHO, as that is exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, HOWEVER! (Hint: update the record using a unique identifier.)
+-- TO DO ***************************************
+
 UPDATE actor
 SET first_name ='GROUCHO'
-WHERE first_name ='HARPO' #MUCHO GROUCHO
+WHERE first_name ='HARPO' AND last_name = 'WILLIAMS'
+OR first_name ='MUCHO GROUCHO'
+WHERE first_name ='HARPO'; 
+-- WHERE first_name ='HARPO' AND last_name = 'WILLIAMS'; #MUCHO GROUCHO
 
+SELECT * FROM actor;
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 SHOW CREATE TABLE address;
 -- 
